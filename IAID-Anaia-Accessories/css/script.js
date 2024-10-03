05a7b20545d9ac46e9c3963e3b8000eb8e772894
aariasul <70024071+aariasul@users.noreply.github.com>
2024-10-03 15:33:22 -0600

    up

---
+++ IAID-Anaia-Accessories/css/script.js
@@ -0,0 +1,28 @@
+function generateLinks() {
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const userLink = document.getElementById('userWhatsappLink');
+    const fixedLink = document.getElementById('fixedWhatsappLink');
+    const fixedURLLink = document.getElementById('fixedURLLink');
+    const personName = document.getElementById('personName').value;
+
+    if (phoneNumber) {
+        const userWhatsappURL = `https://wa.me/${phoneNumber}?text=https://inter-activeid.com/IAID-Anaia-Accessories/index.html`;
+        const fixedWhatsappURL = `https://wa.me/${phoneNumber}?text=https://inter-activeid.com/IAID-Anaia-Accessories/IAID-Anaia-Accessories-Ana-Iris.pdf`;
+        const fixedURLWhatsappURL = `https://wa.me/50687104453?text=Name: ${encodeURIComponent(personName)}%0APhone: ${encodeURIComponent(phoneNumber)}`;
+
+        userLink.href = userWhatsappURL;
+        fixedLink.href = fixedWhatsappURL;
+        fixedURLLink.href = fixedURLWhatsappURL;
+
+        userLink.textContent = `Enviar enlace IAID`;
+        userLink.classList.remove('hidden-btn');
+        userLink.classList.add('visible-btn');
+
+        fixedLink.textContent = `Enviar PDF`;
+        fixedLink.classList.remove('hidden-btn');
+        fixedLink.classList.add('visible-btn');
+
+        fixedURLLink.classList.remove('hidden-btn');
+        fixedURLLink.classList.add('visible-btn');
+    }
+}
