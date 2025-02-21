1010acab0079540520709db2133a98d5dee48bd7
aariasul <70024071+aariasul@users.noreply.github.com>
2025-02-21 00:10:13 -0600

    up

--- IAID-GabyKelly/css/script.js
+++ IAID-GabyKelly/css/script.js
@@ -4,11 +4,12 @@ function generateLinks() {
     const fixedLink = document.getElementById('fixedWhatsappLink');
     const fixedURLLink = document.getElementById('fixedURLLink');
     const personName = document.getElementById('personName').value;
+    const generateButton = document.querySelector('.btn[onclick="generateLinks()"]');
 
     if (phoneNumber) {
-        const userWhatsappURL = `https://wa.me/${phoneNumber}?text=https://inter-activeid.com/IAID-Paris-Inteme/index.html`;
-        const fixedWhatsappURL = `https://wa.me/${phoneNumber}?text=https://inter-activeid.com/IAID-EduCreativos/IAID-Paris-Inteme-Daniela-Picado`;
-        const fixedURLWhatsappURL = `https://wa.me/50672838310?text=Name: ${encodeURIComponent(personName)}%0APhone: ${encodeURIComponent(phoneNumber)}`;
+        const userWhatsappURL = `https://wa.me/${phoneNumber}?text=https://inter-activeid.com/IAID-GabyKelly/index.html`;
+        const fixedWhatsappURL = `https://wa.me/${phoneNumber}?text=https://inter-activeid.com/IAID-GabyKelly/IAID-Gaby-Kelly.pdf`;
+        const fixedURLWhatsappURL = `https://wa.me/50687856399?text=Name: ${encodeURIComponent(personName)}%0APhone: ${encodeURIComponent(phoneNumber)}`;
 
         userLink.href = userWhatsappURL;
         fixedLink.href = fixedWhatsappURL;
@@ -24,5 +25,51 @@ function generateLinks() {
 
         fixedURLLink.classList.remove('hidden-btn');
         fixedURLLink.classList.add('visible-btn');
+
+        // Hide the "Generate Links" button
+        generateButton.style.display = 'none';
     }
+}
+
+
+
+// go back button
+function goBack() { window.history.back(); }
+
+
+
+
+// Add to Home Screen functionality
+let deferredPrompt;
+
+// Listen for the beforeinstallprompt event and save it
+window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('beforeinstallprompt event fired');  // For debugging
+    e.preventDefault();
+    deferredPrompt = e; // Store the event so it can be triggered later
+});
+
+// Function to trigger the install prompt
+function addToHomeScreen() {
+    if (deferredPrompt) {
+        deferredPrompt.prompt(); // Show the install prompt to the user
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the install prompt');
+            } else {
+                console.log('User dismissed the install prompt');
+            }
+            deferredPrompt = null; // Reset the prompt
+        });
+    }
+}
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/IAID-Latitud-Norte/service-worker.js')
+        .then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch(function(error) {
+            console.log('Service Worker registration failed:', error);
+        });
 }
