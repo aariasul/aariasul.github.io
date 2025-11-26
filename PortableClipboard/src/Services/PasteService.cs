a8b13ff70b0c05e14c3f2fc35f36ca402d53f9de
aariasul <70024071+aariasul@users.noreply.github.com>
2025-11-25 18:33:08 -0600

    Create PasteService.cs

---
+++ PortableClipboard/src/Services/PasteService.cs
@@ -0,0 +1,57 @@
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace PortableClipboard.Services
+{
+    public static class PasteService
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct INPUT { public uint type; public INPUTUNION u; }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct INPUTUNION { [FieldOffset(0)] public KEYBDINPUT ki; }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        const uint INPUT_KEYBOARD = 1;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        const ushort VK_CONTROL = 0x11;
+        const ushort VK_V = 0x56;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        public static void SendCtrlV()
+        {
+            var inputs = new INPUT[]
+            {
+                Down(VK_CONTROL),
+                Down(VK_V),
+                Up(VK_V),
+                Up(VK_CONTROL)
+            };
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        private static INPUT Down(ushort key) => new INPUT
+        {
+            type = INPUT_KEYBOARD,
+            u = new INPUTUNION { ki = new KEYBDINPUT { wVk = key } }
+        };
+
+        private static INPUT Up(ushort key) => new INPUT
+        {
+            type = INPUT_KEYBOARD,
+            u = new INPUTUNION { ki = new KEYBDINPUT { wVk = key, dwFlags = KEYEVENTF_KEYUP } }
+        };
+    }
+}
