8e6f724f64a58e578bb55250ce771ee48e5a078d
aariasul <70024071+aariasul@users.noreply.github.com>
2026-05-28 14:52:31 -0600

    up

--- IAID-Kenneth-Bolanos-Fotografia/service-worker.js
+++ IAID-Kenneth-Bolanos-Fotografia/service-worker.js
@@ -1,31 +1,7 @@
-/* ======================
-   SERVICE WORKER MODULE
-====================== */
-
-const CACHE_NAME = "kenneth-bolanos-card-v1";
-
-self.addEventListener("install", function (event) {
-  console.log("Kenneth Bolaños Service Worker installed.");
-
-  self.skipWaiting();
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installing.');
 });
 
-self.addEventListener("activate", function (event) {
-  console.log("Kenneth Bolaños Service Worker activated.");
-
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames
-          .filter(function (cacheName) {
-            return cacheName !== CACHE_NAME;
-          })
-          .map(function (cacheName) {
-            return caches.delete(cacheName);
-          })
-      );
-    }).then(function () {
-      return self.clients.claim();
-    })
-  );
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating.');
 });
