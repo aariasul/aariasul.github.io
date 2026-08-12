825948e25512b0d7635cb7b00fe5c6160cc9d59d
aariasul <70024071+aariasul@users.noreply.github.com>
2026-08-12 16:52:11 -0600

    up

--- IAID-CorporatumCR/service-worker.js
+++ IAID-CorporatumCR/service-worker.js
@@ -1,9 +1,9 @@
 self.addEventListener("install", function () {
-  console.log("Mi Graduación CR service worker installing.");
+  console.log("Corporatum CR service worker installing.");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", function (event) {
-  console.log("Mi Graduación CR service worker activating.");
+  console.log("Corporatum CR service worker activating.");
   event.waitUntil(self.clients.claim());
 });
